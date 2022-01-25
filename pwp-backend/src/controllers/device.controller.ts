0eb1fb37e1916c210367b9a40db13b4e8645bdae
Sina Schnebelt <sina-schnebelt@web.de>
2022-01-25 23:28:28 +0100

    Kommentare

--- pwp-backend/src/controllers/device.controller.ts
+++ pwp-backend/src/controllers/device.controller.ts
@@ -1,27 +1,34 @@
-import { Body, Controller, Get, Post, Route, Security, Tags, Example, Response, Delete, Request, Query  } from 'tsoa';
+import { Body, Controller, Get, Post, Route, Security, Tags, Example, Response, Delete, Request, Query, SuccessResponse  } from 'tsoa';
 import { IError } from '../models/error.model';
-import { Device, IDevice } from '../models/device.model';
+import {  IDevice } from '../models/device.model';
 import { DeviceService, DeviceDeleteParams, DeviceCreateParams } from '../services/device.service';
-
 
 @Route('devices')
 @Tags('Devices')
 
 export class DeviceController extends Controller {
-//@Security('jwt', ['customer'])
 
-/* @Response<IError>(401, 'Unauthorized', {
-    message: 'No token provided',
-    }) */
-
+//Create Device Function is only allowed with token
+@Security('jwt', ['customer'])
 //Request to create a save a New Device into DB By HubId and Thing Desctiption
 @Post('createDevice')
-  public async createDevice(@Body() requestBody: DeviceCreateParams): Promise<IDevice> {
+@SuccessResponse('201', 'Device Created')
+@Response<IError>(400, 'Bad Request', {
+  message: 'HubId or Thing Description in wrong Format',
+})
+public async createDevice(@Body() requestBody: DeviceCreateParams): Promise<IDevice> {
     const response: IDevice = await new DeviceService().createDevice(requestBody);
     return response;
   } 
+
+//Create Device Function is only allowed with token
+@Security('jwt', ['customer'])
 //Delete device with HubId and DeviceId
 @Delete('deleteDevice')
+@SuccessResponse('201', 'Device sucessfully removed')
+@Response<IError>(400, 'Bad Request', {
+  message: 'Could not delete Device',
+})
   public async deleteDevice(@Body() requestBody: DeviceDeleteParams): Promise<IDevice> {
     const response = await new DeviceService().deleteDevice(requestBody);
     this.setStatus(200);
@@ -34,12 +41,10 @@ export class DeviceController extends Controller {
     const response = await new DeviceService().getDevice(hubId);
     return response;
   }  
-
 //Request to get the Device with all Properties By DeviceName
 @Get('getDeviceDetails')
-  public async getDeviceDetails( @Query()  deviceName?: string): Promise<IDevice>  {
+  public async getDeviceDetails(@Query()  deviceName?: string): Promise<IDevice>  {
     const response = await new DeviceService().getDetails(deviceName);
     return response;
   }  
-
 }

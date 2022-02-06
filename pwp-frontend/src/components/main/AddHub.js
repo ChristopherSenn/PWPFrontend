c5e6e01ad3affd59d7c0f4d2d554e334b00fd747
Pia Hammer <piahammer9@gmail.com>
2022-02-06 17:51:42 +0100

    Revert "Mqtt validate"

--- pwp-frontend/src/components/main/AddHub.js
+++ pwp-frontend/src/components/main/AddHub.js
@@ -68,7 +68,7 @@ export default function AddHub() {
         headers: authHeader()
       };
         await axios.post(
-        'http://localhost:4500/hubs/createHub',
+        'http://pwp21.medien.ifi.lmu.de:4500/hubs/createHub',
         createdHubInfos, config
         )
         navigate('/dashboard');

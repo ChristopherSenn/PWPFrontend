948d08cd892950dac24480d6216b1d014f80fdab
Pia Isabel Hammer <piahammer9@gmail.com>
2022-02-19 15:06:06 +0100

    Change port frontend to 54500

--- pwp-frontend/src/actions/hubsActions.js
+++ pwp-frontend/src/actions/hubsActions.js
@@ -7,7 +7,7 @@ export const getAllHubsFromDB = async () => {
     const requestOptions = {
       headers: authHeader()
     };
-    return await axios.get('http://pwp21.medien.ifi.lmu.de:4500/hubs', requestOptions)
+    return await axios.get('http://pwp21.medien.ifi.lmu.de:54500/hubs', requestOptions)
   } catch(error){
     console.log(error.message)
   }
@@ -15,7 +15,7 @@ export const getAllHubsFromDB = async () => {
 
 export const deleteHub = async (hubId) => {
   try{
-    return await axios.delete('http://pwp21.medien.ifi.lmu.de:4500/hubs/deleteHub', {
+    return await axios.delete('http://pwp21.medien.ifi.lmu.de:54500/hubs/deleteHub', {
       headers: authHeader(),
       data: {
         "hubId": hubId
